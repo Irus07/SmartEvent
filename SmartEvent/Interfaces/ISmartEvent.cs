22962bed57a88264f08134cd0f3900137635e568
Irus <cc>
2025-07-12 20:39:45 +0300

    Added descriptions

---
+++ SmartEvent/Interfaces/ISmartEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartEvent.Interfaces
+{
+    public interface ISmartEvent<TDelegate> where TDelegate : Delegate
+    {
+        void Invoke(params object[]? TDelegateParameters);
+        void Subscribe(TDelegate @delegate);
+        void Unsubscribe(TDelegate @delegate);
+    }
+}
--- SmartEvent/SmartEvent.cs
+++ SmartEvent/SmartEvent.cs
@@ -1,118 +1,152 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
+using SmartEvent.Interfaces;
 
 namespace SmartEvent.Classes
 {
-    public partial class SmartEvent<TDelegate>
-        where TDelegate : Delegate
-    {
-        #region Constructors
-        public SmartEvent(params TDelegate[] delegates)
-        {
+    public partial class SmartEvent<TDelegate> :ISmartEvent<TDelegate>
+		where TDelegate : Delegate
+	{
+		#region Constructors
+		public SmartEvent(params TDelegate[] delegates)
+		{
 
-        }
-        public SmartEvent()
-        {
+		}
+		public SmartEvent()
+		{
 
-        }
-        #endregion
+		}
+		#endregion
 
-        private List<Type> _sortedDelegate = new List<Type>();
-        private List<Type> _delegate = new List<Type>();
-        private List<Type> _pinned = new List<Type>();
-        private Dictionary<TDelegate, int> _InvokeMultiple = new Dictionary<TDelegate, int>();
-        private int _count = 0;
-
-
-
-        public void Subscribe(TDelegate @delegate)
-        {
-            Subscribe(@delegate, 5);
-        }
-        public void Subscribe(TDelegate @delegate, ushort priority)
-        {
-
-            _delegate.Add(
-                new Type(
-                    @delegate,
-                    @delegate.GetMethodInfo().GetHashCode(),
-                    _count,
-                    priority));
+		private List<Type> _sortedDelegate = new List<Type>();
+		private List<Type> _delegate = new List<Type>();
+		private List<Type> _pinned = new List<Type>();
+		private Dictionary<TDelegate, int> _InvokeMultiple = new Dictionary<TDelegate, int>();
+		private int _count = 0;
 
 
-            _count++;
+		/// <summary>
+		/// Subscribes a method to an event
+		/// </summary>
+		/// <param name="delegate"></param>
+		public void Subscribe(TDelegate @delegate)
+		{
+			Subscribe(@delegate, 5);
+		}
+		/// <summary>
+		/// Subscribes a method to an event indicating the priority of the call
+		/// </summary>
+		/// <param name="delegate"></param>
+		/// <param name="priority">Sets the priority of the call inside the event</param>
+		public void Subscribe(TDelegate @delegate, ushort priority)
+		{
 
-            Sort();
-        }
-
-        private void Sort()
-        {
-            var rez = from t in _delegate
-                      orderby t.index ascending
-                      orderby t.priority descending
-                      select t;
-
-            _sortedDelegate = rez.ToList();
-        }
-
-        public void Unsubscribe(TDelegate @delegate)
-        {
-            var rez = from t in _delegate
-                      where t.hashCode != @delegate.GetHashCode()
-                      select t;
-
-            _delegate = rez.ToList();
-
-            Sort();
-
-        }
-        public void UnsubscribeAll()
-        {
-            _sortedDelegate = new List<Type> { };
-            _delegate = new List<Type>();
-        }
-        public void AddToMultiQueue(TDelegate @delegate, ushort quantity = 1)
-        {
-            if (quantity == 0)
-                return;
-
-            _InvokeMultiple.Add(@delegate, quantity);
-        }
-
-        public void Invoke(params object[]? TDelegateParameters)
-        {
-            foreach (var item in _InvokeMultiple)
-            {
-                if (item.Value > 0)
-                {
-                    item.Key.DynamicInvoke(TDelegateParameters);
-                    _InvokeMultiple[item.Key] = item.Value - 1;
-                }
-                else
-                {
-                    _InvokeMultiple.Remove(item.Key);
-                }
-            }
-
-            foreach (var item in _sortedDelegate)
-            {
-                item.@delegate.DynamicInvoke(TDelegateParameters);
-            }
-        }
+			_delegate.Add(
+				new Type(
+					@delegate,
+					@delegate.GetMethodInfo().GetHashCode(),
+					_count,
+					priority));
 
 
-        public static SmartEvent<TDelegate> operator +(SmartEvent<TDelegate> smartEvent, TDelegate @delegate)
-        {
-            smartEvent.Subscribe(@delegate);
-            return smartEvent;
-        }
-        public static SmartEvent<TDelegate> operator -(SmartEvent<TDelegate> smartEvent, TDelegate @delegate)
-        {
-            smartEvent.Unsubscribe(@delegate);
-            return smartEvent;
-        }
+			_count++;
+
+			Sort();
+		}
+
+		private void Sort()
+		{
+			var rez = from t in _delegate
+					  orderby t.index ascending
+					  orderby t.priority descending
+					  select t;
+
+			_sortedDelegate = rez.ToList();
+		}
+		/// <summary>
+		/// Deletes the passed handler
+		/// </summary>
+		/// <param name="delegate"></param>
+		public void Unsubscribe(TDelegate @delegate)
+		{
+			var rez = from t in _delegate
+					  where t.hashCode != @delegate.GetHashCode()
+					  select t;
+
+			_delegate = rez.ToList();
+
+			Sort();
+
+		}
+		/// <summary>
+		/// Deletes all handlers.Does not affect pinned handlers
+		/// </summary>
+		public void UnsubscribeAll()
+		{
+			_sortedDelegate = new List<Type> { };
+			_delegate = new List<Type>();
+		}
+		/// <summary>
+		/// Adds a handler with a limited number of calls
+		/// </summary>
+		/// <param name="delegate"></param>
+		/// <param name="quantity">Number of calls</param>
+		public void AddToMultiQueue(TDelegate @delegate, ushort quantity = 1)
+		{
+			if (quantity == 0)
+				return;
+
+			_InvokeMultiple.Add(@delegate, quantity);
+		}
+		/// <summary>
+		/// Calls signed handlers
+		/// </summary>
+		/// <param name="TDelegateParameters">Event call parameters</param>
+		public void Invoke(params object[]? TDelegateParameters)
+		{
+
+			foreach( var t in _pinned)
+				t.@delegate.DynamicInvoke(TDelegateParameters);
+			
+
+			foreach (var item in _InvokeMultiple)
+			{
+				if (item.Value > 0)
+				{
+					item.Key.DynamicInvoke(TDelegateParameters);
+					_InvokeMultiple[item.Key] = item.Value - 1;
+				}
+				else
+				{
+					_InvokeMultiple.Remove(item.Key);
+				}
+			}
+
+			foreach (var item in _sortedDelegate)
+				item.@delegate.DynamicInvoke(TDelegateParameters);
+		}
 
 
-        
-    }
+		public void Pin(TDelegate @delegate)
+		{
+			_pinned.Add(new Type(
+				@delegate,
+				@delegate.GetHashCode(),
+				1,
+				1
+				));
+		}
+
+		public static SmartEvent<TDelegate> operator +(SmartEvent<TDelegate> smartEvent, TDelegate @delegate)
+		{
+			smartEvent.Subscribe(@delegate);
+			return smartEvent;
+		}
+		public static SmartEvent<TDelegate> operator -(SmartEvent<TDelegate> smartEvent, TDelegate @delegate)
+		{
+			smartEvent.Unsubscribe(@delegate);
+			return smartEvent;
+		}
+
+	}
 }
